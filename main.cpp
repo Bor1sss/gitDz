96892446fd0abde48086466af361773e6d2259a9
Boris <solt2003boris@gmail.com>
2022-09-06 19:12:38 +0300

    Удалил класс Person

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
-class Person {
-	string name;
-	int age;
+
+class Point {
+	int x, y;
 public:
-
-
+	Point() {
+		x = y = 0;
+	}
+	Point(int a, int b) {
+		x = a;
+	}
 };
-
 
 
 int main() {
