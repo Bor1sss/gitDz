9a29a7be1727c70c4e48be66da1e2ae80aa5eac1
Boris <solt2003boris@gmail.com>
2022-09-06 19:25:42 +0300

    Финальная версия

--- main.cpp
+++ main.cpp
@@ -3,16 +3,31 @@ using namespace std;
 
 class Point {
 	int x, y;
+
+class student {
+	string name;
+	int age;
+	string name;
+	int age;
+	string name;
+	int age;
 public:
-	Point() {
-		x = y = 0;
+	student() {
+
 	}
-	Point(int a, int b) {
-		x = a;
+	student(string n, int a) {
+		name = n;
+		age = a;
 	}
+
+
+	student(string n, int a) {
+		name = n;
+		age = a;
+	}
+
+
 };
-
-
 int main() {
 
 
